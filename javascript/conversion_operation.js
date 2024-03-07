a475756cbf563dc223d8ac9fb337e01a5116dadd
Abhisheks1103 <162470389+Abhisheks1103@users.noreply.github.com>
2024-03-07 04:35:11 +0000

    conversion of operation

---
+++ javascript/conversion_operation.js
@@ -0,0 +1,24 @@
+let score= true
+
+console.log(typeof (score))
+
+let valueInNumber= Number (score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN(not a number)
+//true=>1,false=>0
+
+let isLoggedIn= "Abhishek"
+
+let booleanLoggedIn= Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// 1=> true, 0=> false
+//"Abhishek"=> true
+
+let num= 33
+let valueInstring= String(num)
+console.log(num);
+console.log(typeof (valueInstring));
